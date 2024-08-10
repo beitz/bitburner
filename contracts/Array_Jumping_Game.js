9bee21de6d8f15e970c3781df73485454e5a82b5
beitz <beitz.t@web.de>
2024-08-10 15:16:41 +0200

    - finished 2 more contract scripts

--- contracts/Array_Jumping_Game.js
+++ contracts/Array_Jumping_Game.js
@@ -2,31 +2,12 @@
 
 import { createTestContract, getContractData, handleContractResult } from 'utils/contractUtils.js';
 
-// Array Jumping Game
-// You are attempting to solve a Coding Contract. You have 1 tries remaining, after which the contract will self-destruct.
-
-
-// You are given the following array of integers:
-
-// 0,6,10,0,5,5,6,1,2,0,3,0,0,2,7
-
-// Each element in the array represents your MAXIMUM jump length at that position. This means that if you are at position i and your maximum jump length is n, you can jump to any position from i to i+n.
-
-// Assuming you are initially positioned at the start of the array, determine whether you are able to reach the last index.
-
-// Your answer should be submitted as 1 or 0, representing true and false respectively.
-
-
-// If your solution is an empty string, you must leave the text box empty. Do not use "", '', or ``.
-
 /**
- * This script solves the "Find Largest Prime Factor" coding contract.
+ * This script solves the "Array Jumping Game" coding contract.
  * 
  * Usage:
- * 1. Test mode: run Find_Largest_Prime_Factor.js test
- * 2. Solve contract: run Find_Largest_Prime_Factor.js <contract_file> <host>
- * 
- * Examples:
+ * 1. Test mode: run Array_Jumping_Game.js test
+ * 2. Solve contract: run Array_Jumping_Game.js <contract_file> <host>
  * 
  */
 
@@ -35,24 +16,46 @@ export async function main(ns) {
     const isTestMode = ns.args[0] === "test";
 
     if (isTestMode) {
-        [contractFile, host, inputData] = createTestContract(ns, "Generate IP Addresses");
+        [contractFile, host, inputData] = createTestContract(ns, "Array Jumping Game");
     } else if (ns.args.length === 2) {
         [contractFile, host, inputData] = getContractData(ns, ns.args[0], ns.args[1]);
     } else {
         ns.tprint("Usage:");
-        ns.tprint("1. Test mode: run Generate_IP_Addresses.js test");
-        ns.tprint("2. Solve contract: run Generate_IP_Addresses.js <contract_file> <host>");
+        ns.tprint("1. Test mode: run Array_Jumping_Game.js test");
+        ns.tprint("2. Solve contract: run Array_Jumping_Game.js <contract_file> <host>");
         return;
     }
     
-    // const solution = do stuff (inputData);
+    const solution = targetReachable(inputData);
 
     if (isTestMode) {
         ns.tprint(`Attempting to solve contract: ${contractFile} on host: ${host} with input: ${inputData} and solution: ${solution}`)
     }
 
     const reward = ns.codingcontract.attempt(solution, contractFile, host);
-    handleContractResult(ns, reward, contractFile, host, isTestMode, "Generate IP Addresses");
+    handleContractResult(ns, reward, contractFile, host, isTestMode, "Array Jumping Game");
 }
 
 // ------------------ functions ------------------
+function targetReachable(inputData) {
+    let maxDistance = 0;
+    let previousMaxDistance = -1;
+
+    while (maxDistance !== previousMaxDistance) {
+        previousMaxDistance = maxDistance;
+
+        // iterate backwards from the current max distance and sum max distance with current element to see if we can reach a new max distance
+        for (let i = maxDistance; i >=0; i--) {
+            if (inputData[i] + i > maxDistance) {
+                maxDistance = inputData[i] + i;
+            }
+        }
+
+        if (previousMaxDistance === maxDistance) { // we're stuck, can't reach the end
+            return 0;
+        }
+        if (maxDistance >= inputData.length - 1) { // we've reached the end!
+            return 1;
+        }
+    }
+}
--- contracts/Array_Jumping_Game_II.js
+++ contracts/Array_Jumping_Game_II.js
@@ -23,10 +23,8 @@ import { createTestContract, getContractData, handleContractResult } from 'utils
  * This script solves the "Find Largest Prime Factor" coding contract.
  * 
  * Usage:
- * 1. Test mode: run Find_Largest_Prime_Factor.js test
- * 2. Solve contract: run Find_Largest_Prime_Factor.js <contract_file> <host>
- * 
- * Examples:
+ * 1. Test mode: run Array_Jumping_Game.js test
+ * 2. Solve contract: run Array_Jumping_Game.js <contract_file> <host>
  * 
  */
 
@@ -35,24 +33,48 @@ export async function main(ns) {
     const isTestMode = ns.args[0] === "test";
 
     if (isTestMode) {
-        [contractFile, host, inputData] = createTestContract(ns, "Generate IP Addresses");
+        [contractFile, host, inputData] = createTestContract(ns, "Array Jumping Game II");
     } else if (ns.args.length === 2) {
         [contractFile, host, inputData] = getContractData(ns, ns.args[0], ns.args[1]);
     } else {
         ns.tprint("Usage:");
-        ns.tprint("1. Test mode: run Generate_IP_Addresses.js test");
-        ns.tprint("2. Solve contract: run Generate_IP_Addresses.js <contract_file> <host>");
+        ns.tprint("1. Test mode: run Array_Jumping_Game_II.js test");
+        ns.tprint("2. Solve contract: run Array_Jumping_Game_II.js <contract_file> <host>");
         return;
     }
     
-    // const solution = do stuff (inputData);
+    const solution = getSteps(ns, inputData);
 
     if (isTestMode) {
         ns.tprint(`Attempting to solve contract: ${contractFile} on host: ${host} with input: ${inputData} and solution: ${solution}`)
     }
 
     const reward = ns.codingcontract.attempt(solution, contractFile, host);
-    handleContractResult(ns, reward, contractFile, host, isTestMode, "Generate IP Addresses");
+    handleContractResult(ns, reward, contractFile, host, isTestMode, "Array Jumping Game II");
 }
 
 // ------------------ functions ------------------
+function getSteps(ns, inputData) {
+    let maxDistance = 0;
+    let previousMaxDistance = -1;
+    let jumps = 0;
+
+    while (maxDistance !== previousMaxDistance) {
+        previousMaxDistance = maxDistance;
+
+        // iterate backwards from the current max distance and sum max distance with current element to see if we can reach a new max distance
+        for (let i = maxDistance; i >=0; i--) {
+            if (inputData[i] + i > maxDistance) {
+                maxDistance = inputData[i] + i;
+            }
+        }
+        jumps++;
+
+        if (previousMaxDistance === maxDistance) { // we're stuck, can't reach the end
+            return 0;
+        }
+        if (maxDistance >= inputData.length - 1) { // we've reached the end!
+            return jumps;
+        }
+    }
+}
